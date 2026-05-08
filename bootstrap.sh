860d2902487dd91c28ed2fd9e420427af9dac731
as_9 <shukaload@gmail.com>
2026-05-08 21:22:42 +0300

    Fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+kind delete cluster --name kind
+kind create cluster --config ./cluster.yml
 kubectl apply -f .infrastructure/mysql/ns.yml
 kubectl apply -f .infrastructure/mysql/configMap.yml
 kubectl apply -f .infrastructure/mysql/secret.yml
